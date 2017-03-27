d9d3cdff2a01ecf87af925f64e99f8cd4f3f5253
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-27 17:45:27 -0400

    create comment migration and model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,9 @@ class CommentsController < ApplicationController
 	def new
 	end
 
+	def create
+	end
+
 	private
 	def comment_params
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 	validates :email, :username, presence:true, uniqueness:true
 	validates :password, length:{minimum:6}
 	validates_confirmation_of :password
+	has_many :comments
 
 end
